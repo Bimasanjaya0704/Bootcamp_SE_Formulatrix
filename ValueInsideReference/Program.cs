fc0a2e023fc2601e80795324f27831885611e5e7
Bimasanjaya0704 <sanjabim77@gmail.com>
2024-07-15 14:31:08 +0700

    [BS] add - add project ValueInsideReference

---
+++ ValueInsideReference/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        Car car = new Car();
+        car.price = 12;
+        Adder add = new();
+        add.Add(car.price);
+        Console.WriteLine(car.price);
+    }
+}
+public class Adder
+{
+    public int Add(int x)
+    {
+        return x++;
+    }
+}
+class Car
+{
+    public int price;
+}
