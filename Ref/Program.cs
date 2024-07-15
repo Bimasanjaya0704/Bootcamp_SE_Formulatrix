0daf22b85175e1aa1b2f0c31228b1a628ecc14c8
Bimasanjaya0704 <sanjabim77@gmail.com>
2024-07-15 14:41:37 +0700

    [BS] add - add project Ref

---
+++ Ref/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        Car car = new Car();
+        car.price = 12;
+        Adder add = new();
+        add.Add(ref car.price);
+        Console.WriteLine(car.price);
+    }
+}
+public class Adder
+{
+    public int Add(ref int x)
+    {
+        return x++;
+    }
+}
+class Car
+{
+    public int price;
+}
