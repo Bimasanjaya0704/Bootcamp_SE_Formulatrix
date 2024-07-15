c49ff886a613d635e8d307e89677479aa8606e0e
Bimasanjaya0704 <sanjabim77@gmail.com>
2024-07-15 13:52:16 +0700

    [BS] add - add project TypeChecker

---
+++ TypeChecker/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+	static void Main()
+	{
+		int a = 12;
+		int b = 22;
+		string sa = "Bima";
+		string sb = "Sanjaya";
+		Calculator calculator = new();
+		int resultInt = (int)calculator.Add(a, b);
+		Console.WriteLine("Hasil result int " + resultInt);
+		string resultString = (string)calculator.Add(sa, sb);
+		Console.WriteLine("Hasil result string " + resultString);
+	}
+}
+
+public class Calculator
+{
+	public object Add(object a, object b)
+	{
+		if (a is int && b is int)
+		{
+			return (int)a + (int)b;
+		}
+		if (a is string && b is string)
+		{
+			return (string)a + (string)b;
+		}
+		return null;
+	}
+}
