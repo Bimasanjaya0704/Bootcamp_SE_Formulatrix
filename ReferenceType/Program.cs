20d9a46fc99cab13b99ccac0678b973a3977be98
Bimasanjaya0704 <sanjabim77@gmail.com>
2024-07-15 14:29:25 +0700

    [BS] add - add project ReferenceType

---
+++ ReferenceType/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Car car = new();
+        car.price = 12;
+        Adder add = new();
+        add.Add(car);
+        Console.WriteLine(car.price);
+    }
+}
+class Adder
+{
+    public int Add(Car x)
+    {
+        return x.price++;
+    }
+}
+class Car
+{
+    public int price;
+}
