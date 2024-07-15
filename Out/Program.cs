306b7965d719ccf85453a51960c989a3f05daaae
Bimasanjaya0704 <sanjabim77@gmail.com>
2024-07-15 14:50:39 +0700

    [BS] add - add project Out

---
+++ Out/Program.cs
@@ -0,0 +1,18 @@
+﻿// Sebelum keluar harus sudah di assign
+public class Program
+{
+    static void Main()
+    {
+        int a;
+        Adder add = new();
+        add.Add(out a);
+        Console.WriteLine(a);
+    }
+}
+public class Adder
+{
+    public int Add(out int x)
+    {
+        return x = 10;
+    }
+}
