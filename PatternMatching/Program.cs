23ce42a660dcc222891f01612c2f54d1048a1f3a
Bimasanjaya0704 <sanjabim77@gmail.com>
2024-07-15 14:00:22 +0700

    [BS] add - add project PatternMatching

--- PatternMatching/Program.cs
+++ PatternMatching/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+	static void Main()
+	{
+		int a = 12;
+		int b = 22;
+		string sa = "Bima";
+		string sb = " Sanjaya";
+		Calculator calculator = new();
+		int resultInt = (int)calculator.Add(a, b);
+		Console.WriteLine("Hasil result int " + resultInt);
+		string resultString = (string)calculator.Add(sa, sb);
+		Console.WriteLine("Hasil result string " + resultString);
+	}
+}
+
+public class Calculator
+{
+	public object Add(object a, object b)
+	{
+		if (a is int c && b is int d)
+		{
+			return c + d;
+		}
+		if (a is string e && b is string f)
+		{
+			return e + f;
+		}
+		return null;
+	}
+}
