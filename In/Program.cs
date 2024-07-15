ebd621e3c87e1a4157fa20cb02c5a3f289f312ad
Bimasanjaya0704 <sanjabim77@gmail.com>
2024-07-15 14:53:14 +0700

    [BS] add - add project In

---
+++ In/Program.cs
@@ -0,0 +1,18 @@
+﻿// Sebelum masuk harus sudah di assign (Read Only)
+public class Program
+{
+    static void Main()
+    {
+        int a = 12;
+        Adder add = new();
+        add.Add(in a);
+        Console.WriteLine(a);
+    }
+}
+public class Adder
+{
+    public void Add(in int x)
+    {
+        // x = 13;
+    }
+}
