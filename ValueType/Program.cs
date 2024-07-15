9b37d459a785aaddda8e91ba91278f8dd7d31130
Bimasanjaya0704 <sanjabim77@gmail.com>
2024-07-15 14:27:35 +0700

    [BS] add - add project ValueType

---
+++ ValueType/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+	static void Main()
+	{
+		int a = 12;
+		Adder add = new();
+		add.Add(a);
+		Console.WriteLine(a);
+	}
+}
+
+public class Adder
+{
+	public int Add(int x)
+	{
+		return x++;
+	}
+}
