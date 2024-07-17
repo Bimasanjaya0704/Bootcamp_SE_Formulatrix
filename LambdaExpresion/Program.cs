a633ab28eacdad5ac20984a26e8f6cd42fda03d8
Bimasanjaya0704 <sanjabim77@gmail.com>
2024-07-17 12:01:42 +0700

    [BS] add - add project delegates

---
+++ LambdaExpresion/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+	static void Main()
+	{
+		Action<string, string> resultString = (string firstName, string lastName) => Console.WriteLine(firstName + " " + lastName);
+		resultString("Bima", "Sanjaya");
+
+		Func<int> resultInt = () => 128;
+		Console.WriteLine(resultInt());
+		Func<int, int, int, int> resultInt2 = (int a, int b, int c) => a + b / c;
+		Console.WriteLine(resultInt2(12, 98, 12));
+	}
+}
