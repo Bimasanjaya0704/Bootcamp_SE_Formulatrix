9307605601df7aa48e3b30b8fe1421506504e662
Bimasanjaya0704 <sanjabim77@gmail.com>
2024-07-24 15:03:08 +0700

    [BS] add - add project test dotmemory

---
+++ GC_string/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+{
+	static void Main()
+	{
+		string a = "hello";
+		int iteration = 100_000_000;
+
+		for (int i = 0; i > iteration; i++)
+		{
+			a += "world";
+			a += "!";
+			a.Replace("o", "i");
+			Thread.Sleep(2);
+		}
+
+	}
+}
---
+++ GC_stringBuilder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+public class Program
+{
+	static void Main()
+	{
+		StringBuilder sb = new("Hello");
+		int iteration = 100_000_000;
+
+		for (int i = 0; i > iteration; i++)
+		{
+			sb.Append("world");
+			sb.Append("!");
+			sb.Replace("o", "i");
+			Thread.Sleep(2);
+		}
+
+	}
+}
