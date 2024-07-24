d8aa3ca83c47bf4f6b49f4b1a1bbe238157ea3fd
Bimasanjaya0704 <sanjabim77@gmail.com>
2024-07-24 13:18:29 +0700

    [BS] add - add try catch finnaly

---
+++ TryCatcyFinnaly/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Program Running");
+        try
+        {
+            int[] myArray = { 1, 2, 3 };
+            Console.WriteLine(myArray[5]);
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Gas terus");
+        }
+        Console.WriteLine("Program Finish");
+    }
+}
