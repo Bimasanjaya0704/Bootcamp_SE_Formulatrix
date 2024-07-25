3f469594854a173939b1777edd71235bea1be7c5
Bimasanjaya0704 <sanjabim77@gmail.com>
2024-07-25 11:54:08 +0700

    [BS] add - add project dispose pattern

---
+++ DisposePattern/Program.cs
@@ -0,0 +1,38 @@
+﻿class MyClass : IDisposable
+{
+	private bool disposedValue;
+	int[] myArray;
+	private FileStream fs;
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!disposedValue)
+		{
+			if (disposing)
+			{
+				// TODO: dispose managed state (managed objects)
+				myArray = null;
+			}
+
+			// TODO: free unmanaged resources (unmanaged objects) and override finalizer
+			// TODO: set large fields to null
+			fs.Dispose();
+			disposedValue = true;
+		}
+	}
+
+	// // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+	// Safety NET
+	~MyClass()
+	{
+		// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+		Dispose(disposing: false);
+	}
+
+	public void Dispose()
+	{
+		// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+		Dispose(disposing: true);
+		GC.SuppressFinalize(this);
+	}
+}
