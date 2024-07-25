a1c7c973062728961dfc28ed5c45b82626b7c750
Bimasanjaya0704 <sanjabim77@gmail.com>
2024-07-25 10:55:48 +0700

    [BS] add - add project finalizers inheritance

---
+++ FinalizersInheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿
+class Foundation
+{
+	~Foundation()
+	{
+		Console.WriteLine("Foundation destructed");
+	}
+}
+
+class House : Foundation
+{
+	~House()
+	{
+		Console.WriteLine("House destructed");
+	}
+}
+class Roof : House
+{
+	~Roof()
+	{
+		Console.WriteLine("Roof destructed");
+	}
+}
+
+public class Program
+{
+	static void Main()
+	{
+		createInctance();
+		GC.Collect();
+		GC.WaitForPendingFinalizers();
+	}
+
+	static void createInctance()
+	{
+		Roof roof = new();
+	}
+}
