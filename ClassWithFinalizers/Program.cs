bb84a6d694ca8885ffdec489cb9d9e63d1ee9f01
Bimasanjaya0704 <sanjabim77@gmail.com>
2024-07-25 11:00:25 +0700

    [BS] add - add project with finalizers and without finalizers

--- ClassWithFinalizers/Program.cs
+++ ClassWithFinalizers/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using System.Diagnostics;
+
+class Foundation
+{
+	~Foundation() { }
+}
+
+class House
+{
+	~House() { }
+}
+
+class Roof
+{
+	~Roof() { }
+}
+
+class Program
+{
+	static void Main()
+	{
+		int iteration = 1_000_000;
+		Stopwatch stopwatch = new();
+		stopwatch.Start();
+		for (int i = 0; i < iteration; i++)
+		{
+			Foundation foundation = new();
+			House house = new();
+			Roof roof = new();
+		}
+		stopwatch.Stop();
+		Console.WriteLine(stopwatch.ElapsedMilliseconds);
+	}
+}
--- ClassWithoutFinalizers/Program.cs
+++ ClassWithoutFinalizers/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+class Foundation
+{
+}
+
+class House
+{
+
+}
+
+class Roof
+{
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        int iteration = 1_000_000;
+        Stopwatch stopwatch = new();
+        stopwatch.Start();
+        for (int i = 0; i < iteration; i++)
+        {
+            Foundation foundation = new();
+            House house = new();
+            Roof roof = new();
+        }
+        stopwatch.Stop();
+        Console.WriteLine(stopwatch.ElapsedMilliseconds);
+    }
+}
