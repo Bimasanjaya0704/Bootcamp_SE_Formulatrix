08d0f450071385d36f6547f8cef4502357f7513a
Bimasanjaya0704 <sanjabim77@gmail.com>
2024-07-25 14:23:48 +0700

    [BS] add - add project using

---
+++ Using/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program : IDisposable
+{
+	static void Main()
+	{
+		TripJakarta();
+	}
+
+	static void TripJakarta()
+	{
+		string FilePath = "MyFile.txt";
+		using (FileStream fs = new(FilePath, FileMode.Create))
+		{
+			HandleTrip(fs);
+		}
+
+	}
+
+	static void HandleTrip(FileStream fs)
+	{
+		//.......
+	}
+
+	public void Dispose()
+	{
+		throw new NotImplementedException();
+	}
+}
